2ef682d34ed7b77535c3e40376a2ccdfc1426ba4
Yuuki Mori <wrong.int1109@gmail.com>
2015-10-26 00:44:38 +0900

    fix some typo

--- install-neutron.sh
+++ install-neutron.sh
@@ -49,7 +49,7 @@ install_packages(){
 
 config_setting_neutron(){
   # database
-  sed -i "/connection = sqlite:\/\/\/\/var\/lib\/neutron\/neutron.sqlite/connection = mysql:\/\/neutron:$PASSWORD@$CONTROLLER\/neutron/" /etc/neutron/neutron.conf
+  sed -i "/connection = sqlite:\/\/\/\/var\/lib\/neutron\/neutron\.sqlite/connection = mysql:\/\/neutron:$PASSWORD@$CONTROLLER\/neutron/" /etc/neutron/neutron.conf
   
   sed -i "/^\[DEFAULT\]/a rpc_backend = rabbit" /etc/neutron/neutron.conf
   sed -i "/^\[DEFAULT\]/a auth_strategy = keystone" /etc/neutron/neutron.conf
@@ -61,17 +61,16 @@ config_setting_neutron(){
   sed -i "/^\[DEFAULT\]/a verbose = True" /etc/neutron/neutron.conf
   sed -i "/^\[DEFAULT\]/a debug = True" /etc/neutron/neutron.conf
 
-  sed -i "/^\[oslo_messaging_rabbit\]/a rabbit_password = $PASSWORD" 
-  /etc/neutron/neutron.conf
+  sed -i "/^\[oslo_messaging_rabbit\]/a rabbit_password = $PASSWORD" /etc/neutron/neutron.conf
   sed -i "/^\[oslo_messaging_rabbit\]/a rabbit_userid = openstack" /etc/neutron/neutron.conf
   sed -i "/^\[oslo_messaging_rabbit\]/a rabbit_host = $CONTROLLER" /etc/neutron/neutron.conf
   
   # keystone_authtoken
-  sed -i "/^auth_uri = http:\/\/127\.0\.0\.1:35357\/v2.0\//auth_uri = http:\/\/$CONTROLLER:35357\/v2\.0\//" /etc/neutron/neutron.conf
+  sed -i "/^auth_uri = http:\/\/127\.0\.0\.1:35357\/v2\.0\//auth_uri = http:\/\/$CONTROLLER:35357\/v2\.0\//" /etc/neutron/neutron.conf
   sed -i "/^identity_uri = http:\/\/127\.0\.0\.1:5000/identity_uri = http:\/\/$CONTROLLER:5000/" /etc/neutron/neutron.conf
   sed -i "/^admin_tenant_name = %SERVICE_TENANT_NAME%/admin_tenant_name = admin_tenant_name = service/" /etc/neutron/neutron.conf
-  sed -i "admin_user = %SERVICE_USER%/admin_user = neutron/" /etc/neutron/neutron.conf
-  sed -i "admin_password = %SERVICE_PASSWORD%/admin_password = $PASSWORD/" /etc/neutron/neutron.conf
+  sed -i "/^admin_user = %SERVICE_USER%/admin_user = neutron/" /etc/neutron/neutron.conf
+  sed -i "/^admin_password = %SERVICE_PASSWORD%/admin_password = $PASSWORD/" /etc/neutron/neutron.conf
   
   sed -i "s/^\[nova\]/#\[nova\]/" /etc/neutron/neutron.conf
   cat <<EOF >> /etc/neutron/neutron.conf
@@ -154,7 +153,7 @@ auth_strategy = keystone
 admin_auth_url = http://$CONTROLLER:35357/v2.0
 admin_tenant_name = service
 admin_username = neutron
-admin_password = N$PASSWORD
+admin_password = $PASSWORD
 EOF
 }
 
@@ -168,6 +167,7 @@ service_restart(){
   done
 }
 
+set_network_parameter()
 create_db
 create_entity
 install_packages
