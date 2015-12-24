d4da9724969d473ef2c6fba2d9f7bc3a73490bc0
Yuuki Mori <wrong.int1109@gmail.com>
2015-12-24 09:29:45 +0900

    add changing mysql bind-address line

--- install-openstack-common.sh
+++ install-openstack-common.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 PASSWORD=password
+CONTROLLER=192.168.0.30
 
 apt install -y ubuntu-cloud-keyring
 add-apt-repository cloud-archive:liberty
@@ -11,6 +12,7 @@ apt install -y mariadb-server python-mysqldb
 sed -i "/^\[mysqld\]/a init-connect = 'SET NAMES utf8'" /etc/mysql/my.cnf
 sed -i "/^\[mysqld\]/a innodb_file_per_table" /etc/mysql/my.cnf
 sed -i "/^\[mysqld\]/a default-storage-engine = innodb" /etc/mysql/my.cnf
+sed -i "s/bind-address[ \f\n\r\t]*=[ \f\n\r\t]*127.0.0.1/bind-address = $CONTROLLER/g" /etc/mysql/my.cnf
 
 service mysql restart
 
